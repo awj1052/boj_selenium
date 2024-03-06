85e5937641d84fc491852ee31bec0d87a95730be
awj1052 <awj1052@naver.com>
2024-03-07 02:44:06 +0900

    refactor: Display the module of ID, PW

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-from AccountData import *
+import AccountData
 
 chrome_options = Options()
 chrome_options.add_argument("--window-size=1920,1080")
@@ -12,9 +12,9 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # 구글 뉴스 페이지 열기
 driver.get('https://www.acmicpc.net/login?next=%2F')
-driver.find_element(By.NAME, 'login_user_id').send_keys(ID)
+driver.find_element(By.NAME, 'login_user_id').send_keys(AccountData.ID)
 element = driver.find_element(By.NAME, 'login_password')
-element.send_keys(PW)
+element.send_keys(AccountData.PW)
 element.send_keys(Keys.ENTER)
 time.sleep(1)
 button = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[2]/div/div[2]/ul/li[7]/a').click() # 그룹
